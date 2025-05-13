ae871872329d7ba721118df1405e805e61bf1a7a
ArKynn <david.f.mendes@proton.me>
2025-05-13 17:39:46 +0100

    Added Lantern Class, Abstract EquipmentObject now also defines Start method.

--- Assets/Scripts/Player/Equipment/EquipmentObject.cs
+++ Assets/Scripts/Player/Equipment/EquipmentObject.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Player
 {
     public abstract class EquipmentObject : MonoBehaviour
     {
+        public abstract void Start();
+
         public abstract void Use();
         
         public abstract void Equip();
---
+++ Assets/Scripts/Player/Equipment/Lantern.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player.Equipment
+{
+    public class Lantern : EquipmentObject
+    {
+        [Header("Light Options")] 
+        [SerializeField] private Color lightColor;
+        [SerializeField] private float maxLightLevel;
+        [SerializeField] private float lanternGainPerUse;
+        [SerializeField] private float lanternLossPerSecond;
+        [SerializeField] private float unequippedLossMultiplier;
+
+        public float LightLevel
+        {
+            get => _lightLevel;
+            private set
+            {
+                _lightLevel = Mathf.Clamp(value, 0f, maxLightLevel); 
+                _animator.SetFloat("LightLevel", _lightLevel);
+                if(_light != null) _light.intensity = _lightLevel;
+            }
+        }
+        
+        private float _lightLevel;
+
+        [Header("Model Options")]
+        [SerializeField] private GameObject viewModel;
+        
+        private Animator _animator;
+        private Light _light;
+        private bool _unequipped = true; 
+        
+        public override void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _light = GetComponentInChildren<Light>();
+            if(_light != null) _light.color = lightColor;
+        }
+
+        private void Update()
+        {
+            if (LightLevel > 0) DimLight();
+        }
+
+        private void DimLight()
+        {
+            var loss = lanternLossPerSecond * Time.deltaTime;
+            if(_unequipped) loss *= unequippedLossMultiplier;
+            LightLevel -= loss;
+        }
+
+        public override void Use()
+        {
+            LightLevel += lanternGainPerUse;
+        }
+
+        public override void Equip()
+        {
+            Use();
+            _unequipped = false;
+            _animator.SetTrigger("Equip");
+        }
+
+        public override void Unequip()
+        {
+            _unequipped = true;
+            _animator.SetTrigger("Unequip");
+        }
+    }
+}
