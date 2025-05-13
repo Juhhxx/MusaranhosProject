f811b4961bf204e360035e4ba646593cee69baf3
ArKynn <david.f.mendes@proton.me>
2025-05-13 16:02:18 +0100

    Added empty InteractiveObject class, PlayerInteraction.Interact calls Detector class

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,12 +1,22 @@
+using Map;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
     [SerializeField] private float interactionRange;
-    [SerializeField] private LayerMask interactivesLayerMask;
+    [SerializeField] private LayerMask interactiveLayerMask;
+    
+    private bool _canInteract;
+
+    public void ToggleInteraction(bool? newState = null)
+    {
+        if(newState is null) _canInteract = !_canInteract;
+        else _canInteract = newState.Value;
+    }
     
     public void Interact()
     {
-        
+        if(!_canInteract) return;
+        var detectedObject = Detector.GetClosestInArea<InteractiveObject>(transform, interactionRange, interactiveLayerMask);
     }
 }
