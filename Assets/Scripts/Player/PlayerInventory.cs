0318708e6a963621f04616a8fd5a0b295bf06180
ArKynn <david.f.mendes@proton.me>
2025-05-14 16:07:08 +0100

    Added Item enum and Inventory class

---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Map;
+
+namespace Player
+{
+    public class PlayerInventory
+    {
+        private Dictionary<Item, int> inventory;
+
+        public void AddItem(Item item)
+        {
+            if (!inventory.TryAdd(item, 1))
+            {
+                inventory[item] += 1;
+            }
+        }
+
+        public bool ContainsItem(Item item)
+        {
+            return inventory.ContainsKey(item);
+        }
+
+        public void RemoveItem(Item item)
+        {
+            if(ContainsItem(item))
+                inventory[item] -= 1;
+        }
+    }
+}
