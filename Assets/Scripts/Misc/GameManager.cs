0862426b949c896460fcc58bc95d1cdc3c1aa913
ArKynn <david.f.mendes@proton.me>
2025-05-14 19:58:01 +0100

    Added GameManager class.

---
+++ Assets/Scripts/Misc/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using Player.Equipment;
+using Compass = Player.Equipment.Compass;
+
+namespace Misc
+{
+    public class GameManager : MonoBehaviour
+    {
+        private Compass compass;
+        private PlayerMovement player;
+        private EnemyMovement enemy;
+        private int dangerLevel;
+
+        public int DangerLevel
+        {
+            get { return dangerLevel; }
+            private set
+            {
+                dangerLevel = value;
+                //enemy.setDangerLevel(value);
+            }
+        }
+
+        private void Start()
+        {
+            compass = FindFirstObjectByType<Compass>();
+            player = FindFirstObjectByType<PlayerMovement>();
+            enemy = FindFirstObjectByType<EnemyMovement>();
+            
+            //enemy.OnBlind() += OnEnemyBlinded;
+            //enemy.OnLostChase() += OnEnemyLostChase;
+            //player.OnScoutMove() += OnPlayerScoutMove;
+        }
+        
+        private void SetCompassTarget(Transform target)
+        {
+            compass.SetNeedleTarget(target);
+        }
+        
+        private void OnEnemyBlinded()
+        {
+            //player.StartChase();
+        }
+
+        private void OnEnemyLostChase()
+        {
+            //player.StartScout();
+        }
+
+        private void OnPlayerScoutMove()
+        {
+            //enemy.Move();
+        }
+    }
+}
