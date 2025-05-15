98c00f6abf02a7490b016aed3551e722425fdcbf
ArKynn <david.f.mendes@proton.me>
2025-05-15 19:45:32 +0100

    Added Hand to Compass Sprite, Updated prefabs, dynamite now plays audio.

--- Assets/Scripts/Map/Dynamite.cs
+++ Assets/Scripts/Map/Dynamite.cs
@@ -5,6 +5,10 @@ namespace Map
 {
     public class Dynamite : InteractiveObject
     {
+        [SerializeField] private PlayAudio litPlayAudio;
+        [SerializeField] private PlayAudio placedPlayAudio;
+        [SerializeField] private PlayAudio explosionPlayAudio;
+        
         private Animator animator;
 
         public void Start()
@@ -12,16 +16,18 @@ namespace Map
             SetRequiredItem(Item.Lighter);
             animator = GetComponent<Animator>();
             animator.SetTrigger("Placed");
+            placedPlayAudio.Play();
         }
 
         public override void Interact()
         {
             animator.SetTrigger("Lit");
+            litPlayAudio.Play();
         }
 
         public void Explosion()
         {
-            
+            explosionPlayAudio.Play();
         }
     }
 }
