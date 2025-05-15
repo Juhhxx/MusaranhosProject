fdea91791db516652099f5fef66bead0f0d43d8f
ArKynn <david.f.mendes@proton.me>
2025-05-15 22:23:39 +0100

    Updated lantern animator and added VoiceMimicry script

---
+++ Assets/Scripts/Enemy/VoiceMimicry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Sound;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace Enemy
+{
+    public class VoiceMimicry : MonoBehaviour
+    {
+        [SerializeField] private IntervaledSoundPlayer danger1Voice;
+        [SerializeField] private IntervaledSoundPlayer danger2Voice;
+        private EnemyController eController;
+        private int dangerLevel;
+        private IntervaledSoundPlayer currentSoundPlayer;
+
+        private void Start()
+        {
+            eController = GetComponent<EnemyController>();
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            if(currentSoundPlayer != null) currentSoundPlayer.StopPlaying();
+        }
+
+        public void StartPlaying()
+        {
+            if(currentSoundPlayer != null) currentSoundPlayer.StartPlaying();
+        }
+    }
+}
