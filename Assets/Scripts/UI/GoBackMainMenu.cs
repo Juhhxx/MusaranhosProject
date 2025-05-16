7454ff3be41dd137c5c32c7c3af9b8e2fa9fe89b
ArKynn <david.f.mendes@proton.me>
2025-05-16 14:58:55 +0100

    :)

--- Assets/Scripts/UI/GoBackMainMenu.cs
+++ Assets/Scripts/UI/GoBackMainMenu.cs
@@ -1,13 +1,27 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace UI
 {
     public class GoBackMainMenu : MonoBehaviour
     {
+        private bool waiting = false;
+
+        private void Update()
+        {
+            if(waiting && Input.anyKeyDown) Menu();
+            
+        }
+
         public void GoBack()
         {
-            SceneManager.LoadScene("MainMenu");
+            waiting = false;
+        }
+
+        private void Menu()
+        {
+                SceneManager.LoadScene("MainMenu");
         }
     }
 }
